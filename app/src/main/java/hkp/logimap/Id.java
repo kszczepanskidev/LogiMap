53b802db1f9077ee3cf5734559ab51c4fbc1e9e4
pawel993 <pawel.plichta@student.put.poznan.pl>
2015-11-15 21:31:53 +0100

    Map tests

--- app/src/main/java/hkp/logimap/Id.java
+++ app/src/main/java/hkp/logimap/Id.java
@@ -1,7 +1,9 @@
 package hkp.logimap;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class Id extends AppCompatActivity {
 
@@ -10,4 +12,10 @@ public class Id extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_id);
     }
+        public void onClickMap(View v)
+    {
+        Intent i = new Intent(getApplicationContext(), MapsActivity.class);
+        startActivity(i);
+    }
+
 }

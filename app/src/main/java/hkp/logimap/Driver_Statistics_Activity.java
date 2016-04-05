01ddd0ca17893b74b0c806af747aa2bfe832a75b
Kamil Szczepański <krysztalzg@gmail.com>
2016-04-05 02:20:24 +0200

    Nice driverstats

--- app/src/main/java/hkp/logimap/Driver_Statistics_Activity.java
+++ app/src/main/java/hkp/logimap/Driver_Statistics_Activity.java
@@ -22,7 +22,12 @@ public class Driver_Statistics_Activity extends AppCompatActivity {
     }
 
     public void runREST() {
-        final TextView tv = (TextView) findViewById(R.id.statisticsView);
+        final TextView driver_name = (TextView) findViewById(R.id.driver_name);
+        final TextView v_km = (TextView) findViewById(R.id.value_kmdone);
+        final TextView v_orders = (TextView) findViewById(R.id.value_orders);
+        final TextView v_packages = (TextView) findViewById(R.id.value_packages);
+        final TextView v_delayed = (TextView) findViewById(R.id.value_delayed);
+
         SharedPreferences preferences = getSharedPreferences("sharedPrefs", MODE_PRIVATE);
 
         Rest_Get api = new Rest_Get(preferences.getString("username", "#"), preferences.getString("password", "#"),
@@ -30,14 +35,36 @@ public class Driver_Statistics_Activity extends AppCompatActivity {
                     @Override
                     public void processFinish(String result) {
                         try {
-                            DriverStats driverSt = new DriverStats(new JSONObject(result));
-                            text = String.format(getResources().getString(R.string.driver_statistics),
-                                    driverSt.km_done, driverSt.orders_done, driverSt.pkg_delivered, driverSt.pkg_delayed);
+                            final DriverStats driverSt = new DriverStats(new JSONObject(result));
 
-                            tv.post(new Runnable() {
+                            driver_name.post(new Runnable() {
                                 @Override
                                 public void run() {
-                                    tv.setText(text);
+                                    driver_name.setText(driverSt.name);
+                                }
+                            });
+                            v_km.post(new Runnable() {
+                                @Override
+                                public void run() {
+                                    v_km.setText(driverSt.km_done.toString());
+                                }
+                            });
+                            v_orders.post(new Runnable() {
+                                @Override
+                                public void run() {
+                                    v_orders.setText(driverSt.orders_done.toString());
+                                }
+                            });
+                            v_packages.post(new Runnable() {
+                                @Override
+                                public void run() {
+                                    v_packages.setText(driverSt.pkg_delivered.toString());
+                                }
+                            });
+                            v_delayed.post(new Runnable() {
+                                @Override
+                                public void run() {
+                                    v_delayed.setText(driverSt.pkg_delayed.toString());
                                 }
                             });
                         } catch (Exception e) {
